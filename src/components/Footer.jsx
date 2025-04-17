dab9e9d0d751d45ea6a0e936aca54d283d6fdb78
JamesHuang <jameshuang145@gmail.com>
2025-04-18 04:10:42 +0800

    add NavLink

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,29 @@
+import { Link, NavLink } from 'react-router';
+
 function Footer() {
+    const footerSiteMapContent = [
+        { to: "/", label: "最新消息" },
+        { to: "/All", label: "全部食間" },
+        { to: "/Wheel", label: "命運食間" },
+        { to: "/MyEatime", label: "我的食間" },
+    ];
+
+    const FooterSiteMapContent = () => (
+        <div className="flex gap-32 font-light p-6 align-middle justify-center">
+            {footerSiteMapContent.map(({to, label}) => (
+                <NavLink
+                    key={to}
+                    to={to}
+                    className={"text-our-sm font-medium"}
+                >
+                    {label}
+                </NavLink>
+            ))}
+        </div>
+    );
+
     return (
+
         <footer className="bg-our-black text-our-white py-4 text-center flex items-center justify-center flex-col h-85">
             <div className="p-6">
                 <p className="text-our-md font-extrabold" >
@@ -9,22 +33,7 @@ function Footer() {
                     Eatime
                 </p>
             </div>
-
-            <div className="flex gap-32 font-light p-6 align-middle justify-center">
-                <a href="#" className="text-our-sm font-medium">
-                    最新消息
-                </a>
-                <a href="#" className="text-our-sm font-medium">
-                    全部食間
-                </a>
-                <a href="#" className="text-our-sm font-medium">
-                    命運食間
-                </a>
-                <a href="#" className="text-our-sm font-medium">
-                    我的食間
-                </a>
-
-            </div>
+            <FooterSiteMapContent/>
             <hr className="border-our-white w-250 p-2.5 " />
             <p className="font-extralight">Copyright belongs to James + Qian team</p>
         </footer>
