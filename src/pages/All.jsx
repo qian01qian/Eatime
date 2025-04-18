5d82fe594621986a993946c0e20d48e78f0a09f7
qian01qian <y143m.ray@gmail.com>
2025-04-18 13:22:36 +0800

    px

--- src/pages/All.jsx
+++ src/pages/All.jsx
@@ -6,15 +6,17 @@ function Full() {
     return (
         <>
             <Header />
-            <Page_banner image="/Fortune.jpg" title="全部食間" />
+            <section className="px-6">
+                <Page_banner image="/Fortune.jpg" title="全部食間" />
 
 
-            <div className="max-w-screen-xl flex flex-row items-center justify-center relative w-full mx-auto overflow-hidden my-10 ">
-                <div className='w-1/3 '><h2>filter</h2></div>
-                <div className="w-2/3 px-6">
-                    <Card_key className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6" />
+                <div className="max-w-screen-xl flex flex-row items-center justify-center relative w-full mx-auto overflow-hidden my-10 ">
+                    <div className='w-1/3 '><h2>filter</h2></div>
+                    <div className="w-2/3 px-6">
+                        <Card_key className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6" />
+                    </div>
                 </div>
-            </div>
+            </section>
             <Footer />
         </>
     );
--- src/pages/Wheel.jsx
+++ src/pages/Wheel.jsx
@@ -3,13 +3,15 @@ import Footer from '../components/Footer'
 import Page_banner from '../components/Page_banner';
 import Feature_Wheel from '../components/Feature_Wheel';
 
-function Wheel (){
+function Wheel() {
     return (
         <>
-            <Header/>
-            <Page_banner image="/wheel.jpg" title="命運食間"/>
-            <Feature_Wheel />
-            <Footer/>
+            <Header />
+            <section className="px-6">
+                <Page_banner image="/wheel.jpg" title="命運食間" />
+                <Feature_Wheel />
+            </section>
+            <Footer />
         </>
     );
-}export default Wheel;
+} export default Wheel;
